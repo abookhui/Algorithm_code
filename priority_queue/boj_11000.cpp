34e7a324d815fb33c27bdaeb8dcaa36f8c995a1a
HanGwonhui <jana32496@gmail.com>
2024-04-10 15:52:15 +0900

    우선순위 큐, 그러디

---
+++ priority_queue/boj_11000.cpp
@@ -0,0 +1,33 @@
+// boj_11000 강의실
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<queue>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    vector<pair<int,int>> v;
+    priority_queue<int,vector<int>,greater<int>> pq;
+    int n,start,end,cnt=0;
+    cin>>n;
+
+    for(int i=0;i<n;i++){
+        cin>>start>>end;
+        v.push_back({start,end});
+    }
+    sort(v.begin(),v.end());
+
+    for(auto w : v){
+        if(!pq.empty()){
+            if(w.first < pq.top()) cnt++;
+            else pq.pop();
+        }
+        else cnt++;
+        pq.push(w.second);
+    }
+    cout<<cnt;
+}
