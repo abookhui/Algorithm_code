b4ca1e656b6bae919300a8e3521626f071dcccd5
HanGwonhui <jana32496@gmail.com>
2024-05-17 15:27:53 +0900

    bfs 특정 위치에서 시작해 특정 거리로 이동

--- Python/graph/boj_13913.py
+++ Python/graph/boj_13913.py
@@ -1,4 +1,6 @@
 # 숨바꼭질4 gold 4
+# 이동 위치 리스트로 저장해서 시간초과 남 >> dict 로 변경하기
+
 from collections import deque
 import sys
 input = sys.stdin.readline
@@ -6,24 +8,30 @@ Size = 100001
 
 n,k = map(int,input().split())
 
-visited = [0] *Size
-
-que = deque([[n , [n]]])
+visited = dict()
+que = deque([n])
+visited[n] = n
 
 while que:
-   
-    x, line = que.popleft()
-    if x == k :
-        print(visited[k])
-        print(' '.join(map(str,line)))
-        break
-    
-    for nx in [x-1,x+1,x*2]:
-        tmp = line[:]
+    x = que.popleft()
 
-        if 0<=nx<Size:
-            if not visited[nx]:
-                visited[nx] = visited[x] + 1
-                tmp.append(nx)
-                que.append([nx,tmp])
+    if x == k:
+        cur = x
+        break
+
+    for nx in [x-1, x+1, x*2]:
+        if 0<=nx<Size and nx not in visited:
+            visited[nx] = x
+            que.append(nx)
+
+ans = deque()
+cnt =0
+
+while visited[cur] != cur:
+    ans.appendleft(cur)
+    cur = visited[cur]
+    cnt+=1
+ans.appendleft(cur)
+print(cnt)
+print(*ans)
         
